dee0a276be211ee1530703c5ec13847152601c84
sspiel4 <sspiel4@gmail.com>
2020-04-15 10:35:30 -0700

    re-organize directory, add histograms for peakyness distributions. create main.py, function.py, and plts.py. main.py generates csv files used by plts.py

---
+++ metered_data/main.py
@@ -0,0 +1,243 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  9 17:09:50 2020
+
+@author: scott
+"""
+
+# dataframe imports
+import pandas as pd
+
+# datetime imports
+from datetime import datetime
+import holidays
+us_holidays = holidays.UnitedStates()
+
+# database imports
+import sqlite3
+import os
+
+# plotly
+#import plotly.graph_objects as go
+#import plotly.express as px
+
+# custom functions import
+from functions import (get_weekdays_df, remove_incomplete_days, day_box, group_days_dict, 
+                       day_lines, normalize, day_sums, df_iwf, df_peakyness)
+
+
+path = "F:\\client\\BPA_E3T\\RCC\\RCCViewer\\"
+filename = "RCC_MV.db"
+# make connection
+con = sqlite3.connect(os.path.join(path, filename))
+
+# =============================================================================
+# #### Peakyness Plot For Sunset ###
+# =============================================================================
+
+df = df_iwf(con, 'sunset')  # custom function to query db
+df = df['2015-01-01':'2020-02-01'] # filter for fully occupied days
+df = remove_incomplete_days(df)
+sunset_flows = df.reset_index()
+
+weekdays = get_weekdays_df(df) # filter for weekdays
+sums_df = day_sums(weekdays) # sum on date
+
+# add three hour peak, volumes, times, and normalized volumes to sums_df
+sums_df = df_peakyness(sums_df, weekdays)
+
+# convert to gallons
+# must be after peakyness function for proper normalization
+sums_df['value'] = sums_df['value']*60 
+
+sunset_peakyness = sums_df.reset_index()
+site = []
+for i in range(0,len(sunset_peakyness)):
+    site.append('sunset')
+
+sunset_peakyness['site'] = site
+
+# =============================================================================
+# #### Peakyness Plot For Stream ###
+# =============================================================================
+
+df = df_iwf(con, 'stream')  # custom function to query db
+df = df['2014-01-01':'2020-02-01'] # filter for fully occupied days
+df = remove_incomplete_days(df)
+stream_flows = df.reset_index()
+
+weekdays = get_weekdays_df(df) # filter for weekdays
+sums_df = day_sums(weekdays) # sum on date
+
+
+# add three hour peak, volumes, times, and normalized volumes to sums_df
+sums_df = df_peakyness(sums_df, weekdays)
+
+
+# convert to gallons
+# must be after peakyness function for proper normalization
+sums_df['value'] = sums_df['value']*60 
+
+stream_peakyness = sums_df.reset_index()
+site = []
+for i in range(0,len(stream_peakyness)):
+    site.append('stream')
+
+stream_peakyness['site'] = site
+
+# =============================================================================
+# #### Peakyness Plot For Yesler ###
+# =============================================================================
+
+df = df_iwf(con, 'yesler')  # custom function to query db
+df = df['2019-07-10':'2020-02-01'] # filter for fully occupied days
+df = remove_incomplete_days(df)
+yesler_flows = df.reset_index()
+
+weekdays = get_weekdays_df(df) # filter for weekdays
+sums_df = day_sums(weekdays) # sum on date
+
+# add three hour peak, volumes, times, and normalized volumes to sums_df
+sums_df = df_peakyness(sums_df, weekdays)
+
+# convert to gallons
+# must be after peakyness function for proper normalization
+sums_df['value'] = sums_df['value']*60 
+
+yesler_peakyness = sums_df.reset_index()
+site = []
+for i in range(0,len(yesler_peakyness)):
+    site.append('yesler')
+
+yesler_peakyness = sums_df.reset_index()
+yesler_peakyness['site'] = site
+
+# =============================================================================
+# #### Peakyness Plot For Block 11 ###
+# =============================================================================
+
+# block 11 needs a custom query because more flows are measured.
+sql_query = 'SELECT * FROM block11_hourly WHERE var = "Flow_CityWater"'
+df = pd.read_sql_query(sql_query, con)
+
+### SORT DATAFRAME SO TIMESTAMP IS INDEX
+# create timestamps
+timestamps = []
+
+for i in range(0, len(df)):
+    # create a datetime object
+    timestamps.append(datetime.fromisoformat(df['time_pt'][i]))
+    
+df['time stamp'] = timestamps        
+# set index to time stamp
+df = df.set_index(['time stamp']).sort_index()
+
+#df = df['2015-01-01':'2020-02-01'] # filter for fully occupied days
+df = remove_incomplete_days(df)
+block11_flows = df.reset_index()
+
+weekdays = get_weekdays_df(df) # filter for weekdays
+sums_df = day_sums(weekdays) # sum on date
+
+# add three hour peak, volumes, times, and normalized volumes to sums_df
+sums_df = df_peakyness(sums_df, weekdays)
+
+# convert to gallons
+# must be after peakyness function for proper normalization
+sums_df['value'] = sums_df['value']*60 
+
+block11_peakyness = sums_df.reset_index()
+site = []
+for i in range(0,len(block11_peakyness)):
+    site.append('block11')
+
+block11_peakyness['site'] = site
+
+# =============================================================================
+# #### Peakyness Plot For Eliz James ###
+# =============================================================================
+
+df = pd.read_csv('ejh_hrly_cwf.csv')
+
+### SORT DATAFRAME SO TIMESTAMP IS INDEX
+# create timestamps
+timestamps = []
+
+for i in range(0, len(df)):
+    # create a datetime object
+    timestamps.append(datetime.fromisoformat(df['time_pt'][i]))
+    
+df['time stamp'] = timestamps        
+# set index to time stamp
+df = df.set_index(['time stamp']).sort_index()
+df = df[:'2019-06-01'].append(df['2019-06-19':'2020-02-01']) # remove flood event
+df = remove_incomplete_days(df)
+ejames_flows = df.reset_index()
+
+# look at weekdays only
+weekdays = get_weekdays_df(df) # filter for weekdays
+sums_df = day_sums(weekdays) # sum on date
+
+# add three hour peak, volumes, times, and normalized volumes to sums_df
+sums_df = df_peakyness(sums_df, weekdays)
+
+# convert to gallons
+# must be after peakyness function for proper normalization
+sums_df['value'] = sums_df['value']*60 
+
+ejames_peakyness = sums_df.reset_index()
+site = []
+for i in range(0,len(ejames_peakyness)):
+    site.append('ejames')
+
+ejames_peakyness['site'] = site
+
+# =============================================================================
+# #### Convert GPM to Gals
+# =============================================================================
+ejames_volume = ejames_flows
+ejames_volume['value'] = ejames_volume['value']*60 # convert to gallons
+
+yesler_volume = yesler_flows
+yesler_volume['value'] = yesler_volume['value']*60 # convert to gallons
+
+stream_volume = stream_flows
+stream_volume['value'] = stream_volume['value']*60 # convert to gallons
+
+sunset_volume = sunset_flows
+sunset_volume['value'] = sunset_volume['value']*60 # convert to gallons
+
+block11_volume = block11_flows
+block11_volume['value'] = block11_volume['value']*60 # convert to gallons
+
+# =============================================================================
+# #### Export data
+# =============================================================================
+
+ejames_volume.to_csv('ejames_volume.csv')
+yesler_volume.to_csv('yesler_volume.csv')
+stream_volume.to_csv('stream_volume.csv')
+sunset_volume.to_csv('sunset_volume.csv')
+block11_volume.to_csv('block11_volume.csv')
+
+ejames_peakyness.to_csv('ejames_peakyness.csv')
+yesler_peakyness.to_csv('yesler_peakyness.csv')
+stream_peakyness.to_csv('stream_peakyness.csv')
+sunset_peakyness.to_csv('sunset_peakyness.csv')
+block11_peakyness.to_csv('block11_peakyness.csv')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ metered_data/plts.py
@@ -0,0 +1,213 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 15 08:49:07 2020
+
+@author: scott
+"""
+
+# dataframe imports
+import pandas as pd
+
+# datetime imports
+from datetime import datetime
+import holidays
+us_holidays = holidays.UnitedStates()
+
+# database imports
+import sqlite3
+import os
+
+# plotly
+from plotly import subplots
+import plotly.graph_objects as go
+import plotly.express as px
+
+# custom functions import
+from functions import (get_weekdays_df, remove_incomplete_days, day_box, group_days_dict, 
+                       day_lines, normalize, day_sums, df_iwf, df_peakyness)
+
+
+# =============================================================================
+# #### Import data
+# =============================================================================
+ejames_volume = pd.read_csv('ejames_volume.csv')
+yesler_volume = pd.read_csv('yesler_volume.csv')
+stream_volume = pd.read_csv('stream_volume.csv')
+sunset_volume = pd.read_csv('sunset_volume.csv')
+block11_volume = pd.read_csv('block11_volume.csv')
+
+ejames_peakyness = pd.read_csv('ejames_peakyness.csv')
+yesler_peakyness = pd.read_csv('yesler_peakyness.csv')
+stream_peakyness = pd.read_csv('stream_peakyness.csv')
+sunset_peakyness = pd.read_csv('sunset_peakyness.csv')
+block11_peakyness = pd.read_csv('block11_peakyness.csv')
+
+
+# =============================================================================
+# #### Scatter Plots
+# =============================================================================
+
+fig = px.scatter(ejames_volume, x='dates', y="value", hover_data=['dates'])
+fig.write_html("ejames_scatter.html")
+
+fig = px.scatter(yesler_volume, x='dates', y="value", hover_data=['dates'])
+fig.write_html("yesler_scatter.html")
+
+fig = px.scatter(stream_volume, x='dates', y="value", hover_data=['dates'])
+fig.write_html("stream_scatter.html")
+
+fig = px.scatter(sunset_volume, x='dates', y="value", hover_data=['dates'])
+fig.write_html("sunset_scatter.html")
+
+fig = px.scatter(block11_volume, x='dates', y="value", hover_data=['dates'])
+fig.write_html("block11_scatter.html")
+
+# =============================================================================
+# #### Cummulative Histograms
+# =============================================================================
+
+x = ejames_peakyness['value']
+fig = go.Figure(data=[go.Histogram(x=x, cumulative_enabled=True, histnorm='probability density')])
+fig.write_html("ejames_hist.html")
+
+x = yesler_peakyness['value']
+fig = go.Figure(data=[go.Histogram(x=x, cumulative_enabled=True, histnorm='probability density')])
+fig.write_html("yesler_hist.html")
+
+x = stream_peakyness['value']
+fig = go.Figure(data=[go.Histogram(x=x, cumulative_enabled=True, histnorm='probability density')])
+fig.write_html("stream_hist.html")
+
+x = sunset_peakyness['value']
+fig = go.Figure(data=[go.Histogram(x=x, cumulative_enabled=True, histnorm='probability density')])
+fig.write_html("sunset_hist.html")
+
+x = block11_peakyness['value']
+fig = go.Figure(data=[go.Histogram(x=x, cumulative_enabled=True, histnorm='probability density')])
+fig.write_html("block11_hist.html")
+
+# =============================================================================
+# #### Peakyness Plots
+# =============================================================================
+
+# peakyness scatterplots for individual buildings
+fig = px.scatter(ejames_peakyness, x='value', y="peak_norm", hover_data=['dates', 'peak_hours'])
+fig.write_html("ejames_peakyness.html")
+
+fig = px.scatter(block11_peakyness, x='value', y="peak_norm", hover_data=['dates', 'peak_hours'])
+fig.write_html("block11_peakyness.html")
+
+fig = px.scatter(yesler_peakyness, x='value', y="peak_norm", hover_data=['dates', 'peak_hours'])
+fig.write_html("yesler_peakyness.html")
+
+fig = px.scatter(stream_peakyness, x='value', y="peak_norm", hover_data=['dates', 'peak_hours'])
+fig.write_html("stream_peakyness.html")
+
+fig = px.scatter(sunset_peakyness, x='value', y="peak_norm", hover_data=['dates', 'peak_hours'])
+fig.write_html("sunset_peakyness.html")
+
+# combo peakyness scatterplots
+df = ejames_peakyness.append(yesler_peakyness).append(stream_peakyness).append(sunset_peakyness).append(block11_peakyness)
+fig = px.scatter(df, x='value', y="peak_norm", color = 'site', 
+                 hover_data=['dates', 'peak_hours', 'site'])
+fig.write_html("sites_peakyness.html")
+
+df = ejames_peakyness.append(yesler_peakyness).append(stream_peakyness).append(sunset_peakyness)
+fig = px.scatter(df, x='value', y="peak_norm", color = 'site', 
+                 hover_data=['dates', 'peak_hours', 'site'])
+fig.write_html("sites_peakyness_noBlock11.html")
+
+
+# =============================================================================
+# #### Peakyness Distributions
+# =============================================================================
+
+# ejames
+fig = subplots.make_subplots(rows=1,
+                  cols=2,
+                  start_cell="bottom-left",
+                  subplot_titles=('Total Volume [gals]', 'Peak Norm'))
+
+x = ejames_peakyness['value']
+trace1 = go.Histogram(x=x, cumulative_enabled=True, histnorm='probability density')
+x = ejames_peakyness['peak_norm']
+trace2 = go.Histogram(x=x, cumulative_enabled=True, histnorm='probability density')   
+
+fig.append_trace(trace1, row=1,col=1)
+fig.append_trace(trace2, row=1,col=2)
+
+fig.update_layout(title_text="ejames peakyness distributions")
+fig.write_html("ejames_peakyness_distribution.html")
+
+# sunset
+fig = subplots.make_subplots(rows=1,
+                  cols=2,
+                  start_cell="bottom-left",
+                  subplot_titles=('Total Volume [gals]', 'Peak Norm'))
+
+x = sunset_peakyness['value']
+trace1 = go.Histogram(x=x, cumulative_enabled=True, histnorm='probability density')
+x = sunset_peakyness['peak_norm']
+trace2 = go.Histogram(x=x, cumulative_enabled=True, histnorm='probability density')
+
+fig.append_trace(trace1, row=1,col=1)
+fig.append_trace(trace2, row=1,col=2)
+
+fig.update_layout(title_text="sunset peakyness distributions")
+fig.write_html("sunset_peakyness_distribution.html")
+
+# yesler
+fig = subplots.make_subplots(rows=1,
+                  cols=2,
+                  start_cell="bottom-left",
+                  subplot_titles=('Total Volume [gals]', 'Peak Norm'))
+
+x = yesler_peakyness['value']
+trace1 = go.Histogram(x=x, cumulative_enabled=True, histnorm='probability density')
+x = yesler_peakyness['peak_norm']
+trace2 = go.Histogram(x=x, cumulative_enabled=True, histnorm='probability density')
+
+fig.append_trace(trace1, row=1,col=1)
+fig.append_trace(trace2, row=1,col=2)
+
+fig.update_layout(title_text="yesler peakyness distributions")
+fig.write_html("yesler_peakyness_distribution.html")
+
+# stream
+fig = subplots.make_subplots(rows=1,
+                  cols=2,
+                  start_cell="bottom-left",
+                  subplot_titles=('Total Volume [gals]', 'Peak Norm'))
+
+x = stream_peakyness['value']
+trace1 = go.Histogram(x=x, cumulative_enabled=True, histnorm='probability density')
+x = stream_peakyness['peak_norm']
+trace2 = go.Histogram(x=x, cumulative_enabled=True, histnorm='probability density')
+
+fig.append_trace(trace1, row=1,col=1)
+fig.append_trace(trace2, row=1,col=2)
+
+fig.update_layout(title_text="stream peakyness distributions")
+fig.write_html("stream_peakyness_distribution.html")
+
+# block11
+fig = subplots.make_subplots(rows=1,
+                  cols=2,
+                  start_cell="bottom-left",
+                  subplot_titles=('Total Volume [gals]', 'Peak Norm'))
+
+x = block11_peakyness['value']
+trace1 = go.Histogram(x=x, cumulative_enabled=True, histnorm='probability density')
+x = block11_peakyness['peak_norm']
+trace2 = go.Histogram(x=x, cumulative_enabled=True, histnorm='probability density')
+
+fig.append_trace(trace1, row=1,col=1)
+fig.append_trace(trace2, row=1,col=2)
+
+fig.update_layout(title_text="block11 peakyness distributions")
+fig.write_html("block11_peakyness_distribution.html")
+
+
+
+
+
